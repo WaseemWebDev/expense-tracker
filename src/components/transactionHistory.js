125c136b590c7d15bb0aad115300dc2da7212a43
WaseemWebDev <waseemyousafzai520@gmail.com>
2020-06-30 09:07:15 -0700

    Update transactionHistory.js

--- src/components/transactionHistory.js
+++ src/components/transactionHistory.js
@@ -11,7 +11,7 @@ export function TransactionHistory() {
             <div className="row justify-content-center">
                 <div className="col-lg-9">
                     <h3>Transaction History</h3>
-                    {!transactions.length  ? <h5 style={{color:"red"}}>No Record yet</h5> :  <h5 style={{color:"green"}}>{transactions.length} Record Found</h5> }
+                    {!transactions.length  ? <h5 style={{color:"red"}}>No Record yet</h5> :  <h5 style={{color:"green"}}>{transactions.length} Records Found</h5> }
                     <br />
                     <ul className="list-group">
                         {transactions.map((transactions, index) => {
